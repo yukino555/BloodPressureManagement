fef56c7e4550ae4fe33ec951bd6b9491676629b5
yukino555 <ex14.u.a.55@gmail.com>
2020-08-20 14:45:43 +0900

    登録画面の登録ボタンを押したら画面が遷移するようにした

--- app/src/main/java/example/com/bloodpressuremanagementuser/BloodPressureActivity.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/BloodPressureActivity.java
@@ -2,13 +2,24 @@ package example.com.bloodpressuremanagementuser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class BloodPressureActivity extends AppCompatActivity {
+public class BloodPressureActivity extends AppCompatActivity
+        implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_pressure);
+        findViewById(R.id.btEntry).setOnClickListener(this);
+    }
+    public void onClick(View view){
+        //ここに遷移するための処理を追加する
+        Intent intent = new Intent(this, UserFinishActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/example/com/bloodpressuremanagementuser/UserFinishActivity.java
@@ -0,0 +1,14 @@
+package example.com.bloodpressuremanagementuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserFinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_finish);
+    }
+}
