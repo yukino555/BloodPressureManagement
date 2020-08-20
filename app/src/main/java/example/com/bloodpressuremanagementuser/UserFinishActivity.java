0caa7ec583009ae892f2879796d3d664952e9722
yukino555 <ex14.u.a.55@gmail.com>
2020-08-20 16:21:02 +0900

    登録完了画面の戻るボタンをタップすると入力画面に戻るようにした

--- app/src/main/java/example/com/bloodpressuremanagementuser/UserFinishActivity.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/UserFinishActivity.java
@@ -3,6 +3,7 @@ package example.com.bloodpressuremanagementuser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class UserFinishActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class UserFinishActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_finish);
+
+    }
+
+    public void onBackButtonClick(View view) {
+        finish();
     }
 }
