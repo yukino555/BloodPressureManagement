03bedd896be6cda6d94a5f5efdf354ff1c346653
yukino555 <ex14.u.a.55@gmail.com>
2020-08-21 11:08:47 +0900

    続、ライフサイクル練習
    テキストp170のメソッドを
    UserFinishActivityに追加した。

--- app/src/main/java/example/com/bloodpressuremanagementuser/UserFinishActivity.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/UserFinishActivity.java
@@ -3,15 +3,47 @@ package example.com.bloodpressuremanagementuser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class UserFinishActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("UserFinish","Sub onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_finish);
 
+    }
+    @Override
+    public void onStart(){
+        Log.i("UserFinish","Sub onStart() called.");
+        super.onStart();
+    }
+    @Override
+    public void onRestart(){
+        Log.i("UserFinish","Sub onRestart() called.");
+        super.onRestart();
+    }
+    @Override
+    public void onResume(){
+        Log.i("UserFinish","Sub onResume() called.");
+        super.onResume();
+    }
+    @Override
+    public void onPause(){
+        Log.i("UserFinish","Sub onPause() called.");
+        super.onPause();
+    }
+    @Override
+    public void onStop(){
+        Log.i("UserFinish","Sub onStop() called.");
+        super.onStop();
+    }
+    @Override
+    public void onDestroy(){
+        Log.i("UserFinish","Sub onDestroy() called.");
+        super.onDestroy();
     }
 
     public void onBackButtonClick(View view) {
