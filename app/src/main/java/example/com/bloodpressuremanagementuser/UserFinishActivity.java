95c9d66b09dc9dd609c0ba60f6e482ad18957d2b
yukino555 <ex14.u.a.55@gmail.com>
2020-08-21 18:06:24 +0900

    登録完了画面の「次へ」ボタンを押したら血圧追加画面に遷移するよう変更しました。

--- app/src/main/java/example/com/bloodpressuremanagementuser/UserFinishActivity.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/UserFinishActivity.java
@@ -2,18 +2,27 @@ package example.com.bloodpressuremanagementuser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
-public class UserFinishActivity extends AppCompatActivity {
+public class UserFinishActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("UserFinish","Sub onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_finish);
+        Button button = findViewById(R.id.btNext);
+        findViewById(R.id.btNext).setOnClickListener(this);
 
+    }
+    public void onClick(View view){
+        //ここに遷移するための処理を追加する
+        Intent intent = new Intent(this, BloodPressureAdditionActivity.class);
+        startActivity(intent);
     }
     @Override
     public void onStart(){
