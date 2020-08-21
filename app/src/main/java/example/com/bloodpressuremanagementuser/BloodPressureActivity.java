51752019a55f903c5d60ea23de39054dcd634060
yukino555 <ex14.u.a.55@gmail.com>
2020-08-21 16:18:37 +0900

    登録ボタンを押したらダイアログが出てくるようにしました。コミットの後マージします。
    ダイアログ内のボタンを押しても次の画面に遷移しないため、次はそれに取り組もうかな。

--- app/src/main/java/example/com/bloodpressuremanagementuser/BloodPressureActivity.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/BloodPressureActivity.java
@@ -1,7 +1,10 @@
 package example.com.bloodpressuremanagementuser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,49 +14,61 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class BloodPressureActivity extends AppCompatActivity{
+    private TextView textView;
+    private DialogFragment dialogFragment;
+    private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i("BloodPressureManegementUser","Main onCreate() called.");
+//        Log.i("BloodPressureManegementUser","Main onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_pressure);
+        Button button = findViewById(R.id.btEntry);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EntryUserInformationDialogFragment dialogFragment = new EntryUserInformationDialogFragment();
+                dialogFragment.show(getSupportFragmentManager(),"EntryUserInformationDialogFragment");
+            }
+        });
+
     // 登録ボタンを押したら画面遷移用の記述
 //        findViewById(R.id.btEntry).setOnClickListener(this);
-        
+
     }
-    @Override
-    public void onStart(){
-        Log.i("BloodPressureManegementUser","Main onStart() called.");
-        super.onStart();
-    }
-    @Override
-    public void onRestart(){
-        Log.i("BloodPressureManegementUser","Main onRestart() called.");
-        super.onRestart();
-    }
-    @Override
-    public void onResume(){
-        Log.i("BloodPressureManegementUser","Main onResume() called.");
-        super.onResume();
-    }
-    @Override
-    public void onPause(){
-        Log.i("BloodPressureManegementUser","Main onPause() called.");
-        super.onPause();
-    }
-    @Override
-    public void onStop(){
-        Log.i("BloodPressureManegementUser","Main onStop() called.");
-        super.onStop();
-    }
-    @Override
-    public void onDestroy(){
-        Log.i("BloodPressureManegementUser","Main onDestroy() called.");
-        super.onDestroy();
-    }
+
+//    @Override
+//    public void onStart(){
+//        Log.i("BloodPressureManegementUser","Main onStart() called.");
+//        super.onStart();
+//    }
+//    @Override
+//    public void onRestart(){
+//        Log.i("BloodPressureManegementUser","Main onRestart() called.");
+//        super.onRestart();
+//    }
+//    @Override
+//    public void onResume(){
+//        Log.i("BloodPressureManegementUser","Main onResume() called.");
+//        super.onResume();
+//    }
+//    @Override
+//    public void onPause(){
+//        Log.i("BloodPressureManegementUser","Main onPause() called.");
+//        super.onPause();
+//    }
+//    @Override
+//    public void onStop(){
+//        Log.i("BloodPressureManegementUser","Main onStop() called.");
+//        super.onStop();
+//    }
+//    @Override
+//    public void onDestroy(){
+//        Log.i("BloodPressureManegementUser","Main onDestroy() called.");
+//        super.onDestroy();
+//    }
     /*
     登録ボタンを押したら画面遷移する処理  public class Blood~manegement に以下をインプリメントする
     implements View.OnClickListener{
-
      */
 //    public void onClick(View view){
 //        //ここに遷移するための処理を追加する
--- app/src/main/java/example/com/bloodpressuremanagementuser/EntryUserInformationDialogFragment.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/EntryUserInformationDialogFragment.java
@@ -16,22 +16,23 @@ public class EntryUserInformationDialogFragment extends DialogFragment {
         builder.setMessage(R.string.dialog_msg);
         builder.setPositiveButton(R.string.dialog_btn_ok, new DialogButtonClickListener());
         builder.setNegativeButton(R.string.dialog_btn_ng, new DialogButtonClickListener());
+
         AlertDialog dialog = builder.create();
         return dialog;
     }
     private class DialogButtonClickListener implements DialogInterface.OnClickListener{
         @Override
         public void onClick(DialogInterface dialog, int which){
-            String msg = "";
+//            String msg = "";
             switch (which){
                 case DialogInterface.BUTTON_POSITIVE:
-                    msg = getString(R.string.dialog_ok_toast);
+//                    msg = getString(R.string.dialog_ok_toast);
                     break;
                 case DialogInterface.BUTTON_NEGATIVE:
-                    msg = getString(R.string.dialog_ng_toast);
+//                    msg = getString(R.string.dialog_ng_toast);
                     break;
             }
-            Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+//            Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
         }
     }
 }
