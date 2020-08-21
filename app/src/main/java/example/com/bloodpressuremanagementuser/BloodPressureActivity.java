a5af071c570a05d3f48117dc7709195f0df75c26
yukino555 <ex14.u.a.55@gmail.com>
2020-08-21 11:04:10 +0900

    ライフサイクル練習
    テキストp169のメソッドを
    BloodPressureActivityに追加した。

--- app/src/main/java/example/com/bloodpressuremanagementuser/BloodPressureActivity.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/BloodPressureActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,9 +14,40 @@ public class BloodPressureActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("BloodPressureManegementUser","Main onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_pressure);
         findViewById(R.id.btEntry).setOnClickListener(this);
+    }
+    @Override
+    public void onStart(){
+        Log.i("BloodPressureManegementUser","Main onStart() called.");
+        super.onStart();
+    }
+    @Override
+    public void onRestart(){
+        Log.i("BloodPressureManegementUser","Main onRestart() called.");
+        super.onRestart();
+    }
+    @Override
+    public void onResume(){
+        Log.i("BloodPressureManegementUser","Main onResume() called.");
+        super.onResume();
+    }
+    @Override
+    public void onPause(){
+        Log.i("BloodPressureManegementUser","Main onPause() called.");
+        super.onPause();
+    }
+    @Override
+    public void onStop(){
+        Log.i("BloodPressureManegementUser","Main onStop() called.");
+        super.onStop();
+    }
+    @Override
+    public void onDestroy(){
+        Log.i("BloodPressureManegementUser","Main onDestroy() called.");
+        super.onDestroy();
     }
     public void onClick(View view){
         //ここに遷移するための処理を追加する
