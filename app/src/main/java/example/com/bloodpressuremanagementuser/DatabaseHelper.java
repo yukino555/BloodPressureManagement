f7cf9a0a832f88d9819c5f3426857fa91a21188c
yukino555 <ex14.u.a.55@gmail.com>
2020-08-24 17:13:15 +0900

    怖いのでコミット。
    https://akira-watson.com/android/sqlite.html
    ここのサンプルコードを打ってます

--- app/src/main/java/example/com/bloodpressuremanagementuser/DatabaseHelper.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/DatabaseHelper.java
@@ -3,16 +3,29 @@ package example.com.bloodpressuremanagementuser;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_BLOODPRESSURE = "bloodpressure.db";
-//    private static final String DATABASE_LOWERBLOODPRESSURE = "lowerbloodpressure.db";
-
     private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Bloodpressure.db";
+    private static final String TABLE_NAME = "bloodpressuredb";
+    private static final String _ID = "_id";
+    private static final String COLUMN_NAME_TITLE = "upperBloodPressure";
+    private static final String COLUMN_NAME_SUBTITLE = "lowerBloodPressure";
 
-    public DatabaseHelper(Context context){
-        super(context,DATABASE_BLOODPRESSURE,null,DATABASE_VERSION);
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + "(" +
+                    _ID + " INTEGER PRIMARY KEY," +
+                    COLUMN_NAME_TITLE + " INTEGER," +
+                    COLUMN_NAME_SUBTITLE + " INTEGER)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    DatabaseHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
     }
+    
     @Override
     public void onCreate(SQLiteDatabase db){
         StringBuilder sb = new StringBuilder();
