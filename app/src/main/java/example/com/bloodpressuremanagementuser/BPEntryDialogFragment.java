adfac97296f954622a6001b594dea3706a3a7de2
yukino555 <ex14.u.a.55@gmail.com>
2020-08-31 17:46:23 +0900

    平均ボタン押したらmaxBPの平均を出したい。途中
    データベースからmaxBPカラムのデータを全部拾う
    拾ったデータを繰り返しながら足して、拾ってきたカラム数（.getCount())で割る

--- app/src/main/java/example/com/bloodpressuremanagementuser/BPEntryDialogFragment.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/BPEntryDialogFragment.java
@@ -8,13 +8,13 @@ import android.os.Bundle;
 import androidx.fragment.app.DialogFragment;
 
 public class BPEntryDialogFragment extends DialogFragment {
-    public Dialog onCreateDialog(Bundle savedInstanceState){
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.dialog_entryMsg);
-        builder.setPositiveButton(R.string.dialog_bt_ok, new DialogButtonClickListener());
-        AlertDialog dialog = builder.create();
-        return dialog;
-    }
+//    public Dialog onCreateDialog(Bundle savedInstanceState){
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//        builder.setMessage(R.string.dialog_entryMsg);
+//        builder.setPositiveButton(R.string.dialog_bt_ok, new DialogButtonClickListener());
+//        AlertDialog dialog = builder.create();
+//        return dialog;
+//    }
     private class DialogButtonClickListener implements DialogInterface.OnClickListener{
         @Override
         public void onClick(DialogInterface dialog, int which){
--- app/src/main/java/example/com/bloodpressuremanagementuser/HomeActivity.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/HomeActivity.java
@@ -14,6 +14,8 @@ public class HomeActivity extends AppCompatActivity {
     SQLiteDatabase db;
     Button btShow;
     TextView textView;
+    TextView textView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,33 +25,60 @@ public class HomeActivity extends AppCompatActivity {
 
         btShow = findViewById(R.id.btShow);
     }
-        public void onShow(View view) {
-            db = helper.getReadableDatabase();
-            Cursor cursor = db.query(
-                    "_BPtable",
-                    new String[]{"_date", "_maxBP", "_minBP", " _pulse"},
-                    null,
-                    null,
-                    null,
-                    null,
-                    null
-            );
-            cursor.moveToFirst();
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < cursor.getCount(); i++) {
-                sb.append(cursor.getString(0));
-                sb.append(" ");
-                sb.append(cursor.getInt(1));
-                sb.append("mmHg");
-                sb.append(cursor.getInt(2));
-                sb.append("mmHg");
-                sb.append(cursor.getInt(3));
-                sb.append("拍/分");
-                sb.append("\n");
-                cursor.moveToNext();
-            }
-            cursor.close();
-            textView = findViewById(R.id.text_view);
-            textView.setText(sb.toString());
+    public void onShow(View view) {
+        db = helper.getReadableDatabase();
+        Cursor cursor = db.query(
+                "_BPtable",
+                new String[]{"_date", "_maxBP", "_minBP", " _pulse"},
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        cursor.moveToFirst();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sb.append(cursor.getString(0));
+            sb.append(" ");
+            sb.append(cursor.getInt(1));
+            sb.append("mmHg");
+            sb.append(cursor.getInt(2));
+            sb.append("mmHg");
+            sb.append(cursor.getInt(3));
+            sb.append("拍/分");
+            sb.append("\n");
+            cursor.moveToNext();
         }
+        cursor.close();
+        textView = findViewById(R.id.text_view);
+        textView.setText(sb.toString());
+    }
+/*
+ データベースに登録したデータを呼び出し平均を出したい
+ */
+    public void onAvg(View view) {
+        db = helper.getReadableDatabase();
+        Cursor cursor = db.query(
+                "_BPtable",
+                new String[]{"_maxBP"},
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        int addMaxBP = 0;
+        int number = cursor.getCount();
+        cursor.moveToFirst();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sb.append(addMaxBP += cursor.getInt(i));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        int avg = addMaxBP/number;
+        textView2 = findViewById(R.id.text_view2);
+        textView2.setText(sb.toString());
+    }
 }
