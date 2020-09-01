852afae721fac1d4e603b4a021cf4babc21780d5
yukino555 <ex14.u.a.55@gmail.com>
2020-09-01 14:11:56 +0900

    平均ボタンを押すと最高血圧、最低血圧、脈拍の平均値が出力されるようにしました。最大値ボタンを押すと、、
    データベースから値を取得し、for文で回して最大値を取得するはずが変数のスコープ？で引っ掛かり上手くいきません。eclipseだとこれでいけるんだけど。。

--- app/src/main/java/example/com/bloodpressuremanagementuser/HomeActivity.java
+++ app/src/main/java/example/com/bloodpressuremanagementuser/HomeActivity.java
@@ -14,8 +14,11 @@ public class HomeActivity extends AppCompatActivity {
     SQLiteDatabase db;
     Button btShow;
     Button btAvg;
+    Button btMaxValue;
+    Cursor cursor;
     TextView textView;
     TextView textView2;
+    TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +29,11 @@ public class HomeActivity extends AppCompatActivity {
 
         btShow = findViewById(R.id.btShow);
         btAvg = findViewById(R.id.btAvg);
-
+        btMaxValue = findViewById(R.id.btMaxValue);
     }
     public void onShow(View view) {
         db = helper.getReadableDatabase();
-        Cursor cursor = db.query(
+        cursor = db.query(
                 "_BPtable",
                 new String[]{"_date", "_maxBP", "_minBP", " _pulse"},
                 null,
@@ -63,9 +66,9 @@ public class HomeActivity extends AppCompatActivity {
 //  maxBPを全部取り出し、繰り返して全部足す
     public void onAvg(View view) {
         db = helper.getReadableDatabase();
-        Cursor cursor = db.query(
+        cursor = db.query(
                 "_BPtable",
-                new String[]{"_maxBP"},
+                new String[]{"_maxBP", "_minBP", "_pulse"},
                 null,
                 null,
                 null,
@@ -73,16 +76,59 @@ public class HomeActivity extends AppCompatActivity {
                 null
         );
         int addMaxBP = 0;
+        int addMinBP = 0;
+        int addPulse = 0;
         int number = cursor.getCount();
         cursor.moveToFirst();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < cursor.getCount(); i++) {
             addMaxBP += cursor.getInt(0);
+            addMinBP += cursor.getInt(1);
+            addPulse += cursor.getInt(2);
             cursor.moveToNext();
         }
         cursor.close();
-        sb.append(addMaxBP/number);
+        sb.append("最高血圧 : ");
+        sb.append(addMaxBP/number + "\n");
+        sb.append("最低血圧 : ");
+        sb.append(addMinBP/number + "\n");
+        sb.append("脈拍 : ");
+        sb.append(addPulse/number + "\n");
         textView2 = findViewById(R.id.text_view2);
         textView2.setText(sb.toString());
+    }
+
+    public void onMaxValue(View view) {
+        db = helper.getReadableDatabase();
+        cursor = db.query(
+                "_BPtable",
+                new String[]{"_maxBP", "_minBP", "_pulse"},
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        cursor.moveToFirst();
+        int maxUpperBP;
+        int maxLowerBP;
+        int maxPulse;
+        int minValue = Integer.MIN_VALUE;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            maxUpperBP = Math.max(minValue,cursor.getInt(0));
+            maxLowerBP = Math.max(minValue,cursor.getInt(1));
+            maxPulse = Math.max(minValue,cursor.getInt(2));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        sb.append("最高血圧 : ");
+        sb.append(maxUpperBP + "\n");
+        sb.append("最低血圧 : ");
+        sb.append(maxLowerBP + "\n");
+        sb.append("脈拍 : ");
+        sb.append(maxPulse + "\n");
+        textView3 = findViewById(R.id.text_view3);
+        textView3.setText(sb.toString());
     }
 }
